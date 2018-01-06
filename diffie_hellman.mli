11595223010be40d40fe2690c9eb1ec9fdda0335
Guillaume Huysmans <ghuysmans99@gmail.com>
2018-01-06 17:05:07 +0100

    Forgot a type annotation in diffie_hellman.mli

--- diffie_hellman.mli
+++ diffie_hellman.mli
@@ -1,7 +1,11 @@
 module Make :
   functor (M : Numbers.S) ->
     sig
-      type parameters = { modulus : M.N.t; generator : M.N.t; bits : int; }
+      type parameters = {
+        modulus: M.N.t;
+        generator: M.N.t;
+        bits: int
+      } [@@deriving yojson]
       val generate : bits:int -> parameters
       val challenge : parameters -> M.N.t * M.N.t
       val derive : parameters -> secret:M.N.t -> M.N.t -> M.N.t
