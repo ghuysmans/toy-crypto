7f9fb358b17cae319a7a7a25627aa0868b2cf57f
Guillaume Huysmans <ghuysmans99@gmail.com>
2021-08-19 04:49:08 +0200

    Implemented DH-based PSI

--- lib/diffie_hellman.ml
+++ lib/diffie_hellman.ml
@@ -17,4 +17,50 @@ module Make (M: Numbers.S) = struct
   let derive {modulus; _} ~secret received_challenge =
     (* on both sides, (g ^ remote) ^ local = g ^ (remote * local) *)
     M.modpow received_challenge secret ~m:modulus
+
+  (* TODO upstream, see https://github.com/ocaml/ocaml/issues/5183 *)
+  (* modern Fisher-Yates, REF??? *)
+  let shuffle a =
+    for i = Array.length a - 1 downto 1 do
+      let j = Random.int (i + 1) in
+      let tmp = a.(j) in
+      a.(j) <- a.(i);
+      a.(i) <- tmp
+    done
+
+  let split a =
+    if Array.length a = 0 then
+      [| |], [| |]
+    else
+      let snds = Array.(make (length a) (snd a.(0))) in
+      Array.mapi (fun i (x, y) -> snds.(i) <- y; x) a, snds
+
+  let hash ({bits; _} as p) plain =
+    let secret = M.random ~bits in
+    let a = Array.map (fun x -> x, Digest.string x |> M.of_string |> derive p ~secret) plain in
+    shuffle a;
+    secret, split a
+
+  let reveal p ~secret a =
+    Array.map (derive p ~secret) a |>
+    Array.to_list
+
+  (* FIXME move into Numbers? *)
+  module S = Set.Make (struct
+    type t = M.N.t
+    let compare x y =
+      let open M.N in
+      if x = y then
+        0
+      else if x <= y then
+        -1
+      else
+        1
+  end)
+
+  let intersection plain ~other back =
+    let s = S.of_list other in
+    List.combine (Array.to_list plain) back |>
+    List.filter (fun (_, code) -> S.mem code s) |>
+    List.map fst
 end
--- lib/diffie_hellman.mli
+++ lib/diffie_hellman.mli
@@ -9,4 +9,9 @@ module Make :
       val generate : bits:int -> parameters
       val challenge : parameters -> M.N.t * M.N.t
       val derive : parameters -> secret:M.N.t -> M.N.t -> M.N.t
+
+      (* FIXME write about this *)
+      val hash : parameters -> string array -> M.N.t * (string array * M.N.t array)
+      val reveal : parameters -> secret:M.N.t -> M.N.t array -> M.N.t list
+      val intersection : string array -> other:M.N.t list -> M.N.t list -> string list
     end
